7baba8e044fdd17aead556b61348f29faacdc211
KimGaeun <greencloud11@hanyang.ac.kr>
2023-01-25 20:44:54 +0900

    solved week2 level1

---
+++ 김가은/WEEK2_크기가 작은 부분 문자열.js
@@ -0,0 +1,9 @@
+function solution(t, p) {
+  const arr = [];
+
+  for (let i = 0; i < t.length - p.length + 1; i++) {
+    arr.push(t.slice(i, i + p.length));
+  }
+
+  return arr.filter((a) => +a <= +p).length;
+}
