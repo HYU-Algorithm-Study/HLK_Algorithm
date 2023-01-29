01710ace32b6b3452b34acab2efda52fcb624e99
Subin <supiao0123@gmail.com>
2023-01-30 01:44:50 +0900

    WEEK 2

--- 이수빈/week2/JadenCase문자열만들기.py
+++ 이수빈/week2/JadenCase문자열만들기.py
@@ -1,11 +1,9 @@
 def solution(s):
     s = s.lower()
-    s = s.replace('  ', ' ')
-    words = s.split()
     answer = ''
-    for word in words:
-        new_word = word.replace(word[0], word[0].upper())
-        answer += new_word
-        answer += ' '
-    answer = answer[:-1]
+    for i in s:
+        if answer == '' or answer[-1] == ' ':
+            answer += i.upper()
+        else:
+            answer += i
     return answer
