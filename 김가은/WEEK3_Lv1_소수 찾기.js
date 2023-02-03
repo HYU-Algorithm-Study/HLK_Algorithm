1705f745312ae49958eb29392d3a10d83e2dd103
KimGaeun <greencloud11@hanyang.ac.kr>
2023-02-03 20:43:01 +0900

    Add WEEK3 lv1 code

--- 김가은/WEEK3_Lv1_소수 찾기.js
+++ 김가은/WEEK3_Lv1_소수 찾기.js
@@ -1,4 +1,4 @@
-// 최종 풀이
+// 최종 풀이 1
 function solution(n) {
   const sqrt = Math.sqrt(n);
 
@@ -11,4 +11,22 @@ function solution(n) {
   }
 
   return arr.filter((a) => a).length - 1;
+}
+
+// 최종 풀이 2
+function solution(n) {
+  const sqrt = Math.sqrt(n);
+  let num = n;
+
+  let arr = Array.from({ length: n }, (_, i) => i + 1);
+
+  for (let i = 2; i <= sqrt; i++) {
+    for (let j = i; j <= n; j += i) {
+      if (j !== i && arr[j - 1]) {
+        arr[j - 1] = "";
+        num--;
+      }
+    }
+  }
+  return num - 1;
 }
